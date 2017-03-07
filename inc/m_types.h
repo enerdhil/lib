15ddd7987b08b24d572ad3c767fd477dfc5a3bd7
Ne02ptzero <louis@ne02ptzero.me>
2017-03-07 21:58:21 +0100

    Fix(Comment): Fix comment style in mtypes

--- inc/m_types.h
+++ inc/m_types.h
@@ -19,14 +19,14 @@
 
 /* Generic types */
 
-typedef signed char			s8_t;
-typedef signed short		s16_t;
-typedef signed int			s32_t;
-typedef signed long long	s64_t;
+typedef signed char         s8_t;
+typedef signed short        s16_t;
+typedef signed int          s32_t;
+typedef signed long long    s64_t;
 
-typedef unsigned char		u8_t;
-typedef unsigned short		u16_t;
-typedef unsigned int		u32_t;
-typedef unsigned long long	u64_t;
+typedef unsigned char       u8_t;
+typedef unsigned short      u16_t;
+typedef unsigned int        u32_t;
+typedef unsigned long long  u64_t;
 
 #endif /* M_TYPES_H */
